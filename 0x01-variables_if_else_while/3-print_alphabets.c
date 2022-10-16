a982bca3ce060c10bd83d8c949a33d93b14ca1ae
BrendonKaruru <brendonkaruru21@gmail.com>
2022-10-16 13:37:01 -0700

    upper and lower case

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - printing alphabet in lower case and upper case
+ * return 0
+ */
+int main(void)
+{
+	char ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+		putchar(ch);
+
+	for (ch = 'A'; ch <= 'Z' ch++)
+		putchar(ch);
+
+	putchar('\n');
+
+	return (0);
+}
