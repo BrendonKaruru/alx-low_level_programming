8f3898d7f1f05c45f6aa2dba12388860a91bb99f
BrendonKaruru <brendonkaruru21@gmail.com>
2022-10-16 13:56:44 -0700

    alphabet again

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - prints alphabet in lowercase
+ * followed by a new line excpect e and q
+ * Return; Always 0
+ */
+int main(void)
+{
+	char la;
+
+	for (la = 'a'; <= 'z'; la++)
+	{
+		if (la != 'e' && la != 'q')
+			putchar(la);
+	}
+	putchar('\n');
+	return (0);
+}
