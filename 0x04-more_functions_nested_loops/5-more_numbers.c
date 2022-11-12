da5f9e8168243cb4361752b98b9c623358ac184f
BrendonKaruru <brendonkaruru21@gmail.com>
2022-11-12 02:14:28 -0800

    5-more_numbers.c

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -8,11 +8,11 @@ void more_numbers(void)
 {
 	int num, a;
 
-	for (a = 0; a <= 9; a++)
+	for (a = 0; a < 10; a++)
 	{
 		for (num = 0; num <= 14; num++)
 		{
-			if (num > 9)
+			if (num >= 10)
 			{
 				_putchar((num / 10) + '0');
 			}
